b258c46f436325e32f7b58ef5ed5e94c8e7f4a34
MukhametDev <zhanbekovm@mail.ru>
2024-09-06 12:31:41 +0500

    5:Подправит стили при адаптивности

--- app/View/templates/home.php
+++ app/View/templates/home.php
@@ -1,29 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo htmlspecialchars(\App\View\View::includeCSS('style.css')); ?>">
     <title><?php echo htmlspecialchars($title); ?></title>
 </head>
+
 <body>
-    <div class="wrapper">
-        <div class="wrapper__blocs">
+    <main class="main">
         <section class="sidebar">
             <div class="sidebar__top">
                 <form action="#" class="sidebar__form">
                     <input placeholder="Наименование директории" type="text" class="sidebar__input">
                     <div class="sidebar__btns">
-                        <button  class="sidebar__btn">Добавить папку</button>
+                        <button class="sidebar__btn">Добавить папку</button>
                         <button class="sidebar__btn">Добавить файл</button>
                         <button class="sidebar__btn">Удалить</button>
                     </div>
                 </form>
             </div>
             <div class="sidebar__bottom">
-<!--                <ul class="sidebar__directories">-->
-                    <?php echo $data['directories'] ?>
-<!--                </ul>-->
+                <?php echo $data['directories'] ?>
             </div>
         </section>
         <section class="content">
@@ -35,9 +34,11 @@
                 <img class="no-img" src="/images/no-photo.png" alt="">
             </div>
         </section>
-        </div>
-    </div>
+    </main>
 
-    <script type="module" src="<?php echo htmlspecialchars(\App\View\View::includeJS('app.js')); ?>"></></script>
+    <script type="module" src="<?php echo htmlspecialchars(\App\View\View::includeJS('app.js')); ?>">
+        
+    </script>
 </body>
+
 </html>
